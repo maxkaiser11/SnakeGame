63cece28e581194295ffb69faa8cec37bb493137
Max <max@kaiser.family>
2024-03-19 09:18:03 +0100

    created game loop and window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "raylib.h"
 
+Color green = {183, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
 int main()
 {
+    InitWindow(750, 750, "Retro Snake");
+    
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
 
+        // Drawing
+        ClearBackground(green);
+
+        EndDrawing();
+    }
+
+
+    CloseWindow();
 }
